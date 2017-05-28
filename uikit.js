241b4a68bfe81267e58184a10b4d6c110048d84f
Vivek Seth <Vivekseth.m@gmail.com>
2017-05-27 17:25:47 -0700

    A lot of stuff.
    Mostly complete View implementation.
    Animation API with custom animation curves
    drawRect with transforms
    other stuff I cannot remember

--- uikit.js
+++ uikit.js
@@ -1,15 +1,22 @@
 // App Init
-docReady(windowDidLoad);
+docReady(function() {
+    // setup window
+    windowDidLoad();
+
+    // Setup Event Listenders
+    window.addEventListener('resize', windowResize);
+    window.rootView = loadRootView();
+})
 
 //! App Delegate
 function windowDidLoad() {
     var body = document.body;
-    // body.style.margin = 0;
-    // body.style.textAlign = 'center';
+    body.style.margin = 0;
+    body.style.textAlign = 'center';
 
-    body.style.borderColor = '#00F';
-    body.style.borderStyle = 'solid';
-    body.style.borderWidth = '10px';
+    // body.style.borderColor = '#00F';
+    // body.style.borderStyle = 'solid';
+    // body.style.borderWidth = '10px';
 
     var canvas = document.createElement('canvas');
     canvas.id = 'main-canvas';
@@ -17,125 +24,241 @@ function windowDidLoad() {
     canvas.style.borderStyle = 'solid';
     canvas.style.borderWidth = '2px';
     canvas.style.posistion = 'fixed'
-
     setFrame(canvas, CGRectInset(getBounds(body), 2, 2));
 
+    // Add event listener for `click` events.
+    canvas.addEventListener('click', function(event) {
+        var x = event.pageX - canvas.offsetLeft;
+        var y = event.pageY - canvas.offsetTop;
+
+        // TODO(vivek): Kick off hitTest here...
+
+    }, false);
+
+
     body.appendChild(canvas);
-
-
-
-    // Setup Event Listenders
-    window.addEventListener('resize', windowResize);   
+    window.canvas = canvas;
 }
 
 function windowResize() {
     var canvas = document.getElementById('main-canvas');
-    setFrame(canvas, CGRectInset(getBounds(document.body), 2, 2));
+    setFrame(canvas, CGRectInset(getBounds(document.body), 20, 20));
 }
 
+function loadRootView() {
+    var rootView = new View();
+    rootView.setFrame(CGRectInset(getBounds(canvas), 100, 100))
+
+    var subview = new View();
+    subview.setFrame(CGRectMake(30, 30, 100, 100));
+    rootView.addSubview(subview);
+
+    var subview2 = new View();
+    subview2.setFrame(CGRectMake(160, 30, 100, 100));
+    rootView.addSubview(subview2);
+
+    subview2.animateWithCurveAndDuration(AnimationCurveInOutQuad, 1.0 * 1000, function(view, t, relativeTimestamp, duration) {
+        // TODO(vivek): can I use an abstraction to do the interpolation for me? How do CA animations work? How do they interpolate transforms? 
+        var angle = t * 2 * Math.PI;
+
+        var frame = view.getFrame();
+        var trans = CGTransformTranslate(-frame.size.width / 2, -frame.size.height / 2);
+        trans = CGTransformConcat(CGTransformRotate(angle), trans);
+        trans = CGTransformConcat(CGTransformTranslate(frame.size.width / 2, frame.size.height / 2), trans);
+        view.setTransform(trans);
+
+        // TODO(vivek): can I coalesce animations to that only one root function is responsible for drawing? 
+        // One root object should be responsible for calling requestAnimationWithBlock
+        // When the callback triggers, it fetches callbacks for all running animatsions and call them to update values. 
+        // Then it redraws the whole canvas. (If possible I should look into only redrawing dirty rects).
+        // Finally it calls requestAnimationBlock IF there are animations that need to continue 
+        // To summarize: 
+        //  1. root object owns requestAnimationBlock calling
+        //  2. When root callback triggers, fetch current animations and purge outdated animations. 
+        //  3. Call all animation blocks to update values
+        //  4. Redraw dirty regions of canvas (for now the whole thing)
+        //  5. If there are remaining animations, schedule another animation block
+        ctx = window.canvas.getContext("2d");
+        ctx.clearRect(0, 0, window.canvas.width, window.canvas.height);
+        window.rootView.drawRect(ctx);
+    })
 
 
-// TODO(vivek): need a new name for my rect class. 
-// TODO(vivek): attempt adopting a more javascripty approach to rects. 
-// TODO(vivek): consider removing namespace to make more javascripty.
+    subview.animateWithCurveAndDuration(AnimationCurveInOutQuad, 1.0 * 1000, function(view, t, relativeTimestamp, duration) {
+        var frame = view.getFrame();
+        var trans = CGTransformTranslate(130 * t, 100 * t);
+        view.setTransform(trans);
+
+        ctx = window.canvas.getContext("2d");
+        ctx.clearRect(0, 0, window.canvas.width, window.canvas.height);
+        window.rootView.drawRect(ctx);
+    })
 
 
-//! CGRect
-function CGRectZero() {
-    return CGRectMake(0, 0, 0, 0);
+    return rootView;
 }
 
-function CGRectNull() {
-    return CGRectMake(null, null, null, null);
+function drawViews(timestamp) {
+   // console.log(timestamp);
+
+    var s = window.rootView._subviews[1];
+    var frame = s.getFrame();
+    var t = CGTransformTranslate(-frame.size.width / 2, -frame.size.height / 2);
+    t = CGTransformConcat(CGTransformRotate(Math.PI * 0.01), t);
+    t = CGTransformConcat(CGTransformTranslate(frame.size.width / 2, frame.size.height / 2), t);
+    s.setTransform(CGTransformConcat(t, s._transform));
+
+    ctx = window.canvas.getContext("2d");
+    ctx.clearRect(0, 0, window.canvas.width, window.canvas.height);
+    window.rootView.drawRect(ctx);
+
+    requestAnimationFrame(drawViews);
 }
 
-function CGRectMake(x, y, width, height) {
-    return {
-        origin: {x: x, y: y},
-        size: {width: width, height: height}
+function View() {
+    // Private
+    this._bounds = CGRectZero();
+    this._frame = CGRectZero();
+    this._subviews = [];
+    this._needsLayout = false;
+    this._transform = CGTransformIdentity(); // TODO(vivek)
+
+    // Public
+    this.superview = null;
+
+    // Private Methods
+    this.setFrameTransform = function(ctx) {
+        // sx kx tx 
+        // ky sy ty
+        //  0  0  1
+
+        // a c e
+        // b d f
+        // 0 0 1
+        ctx.transform(1, 0, 0, 1, this._frame.origin.x, this._frame.origin.y)
     }
-}
 
-function CGRectFromBBox(bbox) {
-    return CGRectMake(bbox.left, bbox.top, bbox.width, bbox.height);
-}
+    // Methods
+    this.drawRect = function(ctx, rect) {
+        ctx.save();
+        this.setFrameTransform(ctx);
+        ctx.transform.apply(ctx, this._transform);
 
-function CGRectInset(rect, xInset, yInset) {
-    if (!rect) {
-        return;
-    }
-    var newRect = JSON.parse(JSON.stringify(rect));
-    newRect.origin.x += xInset;
-    newRect.origin.y += yInset;
-    newRect.size.width -= 2 * xInset;
-    newRect.size.height -= 2 * yInset;
-    return newRect
-}
-
-function CGRectRelativeToRect(sourceRect, relativeRect) {
-    var newRect = JSON.parse(JSON.stringify(relativeRect));
-    newRect.origin.x -= sourceRect.origin.x;
-    newRect.origin.y -= sourceRect.origin.y;
-    return newRect;
-}
-
-
-
-//! Dom Node Helpers
-
-function setFrame(node, rect) {
-    if (!node) {
-        return;
-    }
-
-    node.style.top = rect.origin.y;
-    node.style.left = rect.origin.x;
-    node.style.width = rect.size.width;
-    node.style.height = rect.size.height;
-}
-
-// TODO(vivek): frame of document.body should be the entire size. its bounds should take care of the visibleRect being smaller. Not sure how to handle resizing a document, obviously subelements should adapt their sizes to fit. 
-function getFrame(node) {
-    // TODO(vivek): there is no concept of a transform so this is easy.
-    if (!node || node == document) {
-        return CGRectNull();
-    }
-    else if (node == document.body.parentNode) {
-        var bbox = node.getBoundingClientRect();
-        return CGRectMake(bbox.left, bbox.top, node.clientWidth, node.clientHeight);
-    }
-    else {
-        var absoluteRect = CGRectFromBBox(node.getBoundingClientRect());
-        if (node == document.body) {
-            absoluteRect.size.height = node.clientHeight;
-            absoluteRect.size.width = node.clientWidth;
+        for (var i=0; i<this._subviews.length; i++) {
+            var subview = this._subviews[i];
+            subview.drawRect(ctx, subview.getFrame());
         }
 
-        var parentRect = CGRectFromBBox(node.parentNode.getBoundingClientRect());
-        return CGRectRelativeToRect(parentRect, absoluteRect);
+        // Red outline
+        ctx.strokeStyle = "#00FF00";
+        ctx.strokeRect(
+            this._bounds.origin.x, 
+            this._bounds.origin.y, 
+            this._bounds.size.width, 
+            this._bounds.size.height);
+
+        ctx.restore();
+    };
+    this.setNeedsLayout = function() {
+        this._needsLayout = true;
+    }
+    this.layoutSubviews = function() {
+
+        // TODO(vivek): need an implementation here..
+        // Might need a queue of things to layout? 
+
+        this._needsLayout = false;
+    };
+
+    this.sizeThatFits = function(size) {return size;}
+    this.getFrame = function() {
+        return CGRectCopy(this._frame);
+    }
+
+    // TODO(vivek): this seems suspect....
+    this.setFrame = function(frame) {
+        this._frame = CGRectCopy(frame);
+        var bounds = CGRectCopy(frame);
+        bounds.origin = CGPointZero();
+        this._bounds = bounds;
+        this.setNeedsLayout();
+    }
+
+    this.getBounds = function() {
+        return CGRectCopy(this._bounds);
+    }
+
+    this.setTransform = function(t) {
+        this._transform = t;
+        this.setNeedsLayout();
+    }
+
+    this.addSubview = function(view) {
+        this._subviews.push(view);
+        view.superview = this;
+        this.setNeedsLayout();
+    }
+
+    this.removeFromSuperview = function() {
+        if (!this.superview) {
+            return;
+        }
+
+        var index = this.superview.indexOf(this);
+        this.superview._subviews.splice(index, 1);
+        this.superview.setNeedsLayout();
+        this.superview = null;
+    }
+
+    // I can modify the function by which t is calculated to create custom animation curves. 
+    this.animateWithDuration = function(duration, callback) {
+        this.animateWithCurveAndDuration(null, duration, callback);
+    }
+
+    this.animateWithCurveAndDuration = function(curve, duration, callback) {
+        curve = curve ? curve : function(x) {return x};
+        var that = this;
+
+        var startTime = null;
+        function step(timestamp) {
+            startTime = startTime ? startTime : timestamp;
+            var relativeTimestamp = (timestamp - startTime);
+            var tLinear = relativeTimestamp / duration;
+
+            t = curve(tLinear);
+
+            console.log(startTime, timestamp, relativeTimestamp, duration, t);
+            callback(that, t, relativeTimestamp, duration);
+            if (relativeTimestamp < duration) {
+                requestAnimationFrame(step);
+            }
+        }
+        requestAnimationFrame(step);
     }
 }
 
-function getBounds(node) {
-    var style = window.getComputedStyle(node);
-    var inset = 0;
-    inset += parseFloat(style.margin);
-    inset += parseFloat(style.borderWidth);
-    inset += parseFloat(style.padding);
+/*
+View
+    - drawRect:
+- layoutSubviews:
+- sizeThatFits
 
-    var frame = getFrame(node);
-    frame = CGRectInset(frame, inset, inset);
-    frame.origin = {x:0, y:0};
+- hitTest
+- pointInside
 
-    return frame;
-}
+- convertXtoView
+- convertXfromView
+
+    - setFrame:
+    - getFrame
+    - getBounds
+- setBounds???
+    - addSubview
+    - removeFromSuperview
+    - superview
 
 
-
-
-
-
-
+*/
 
 
 
--- util.js
+++ util.js
@@ -70,3 +70,204 @@
         }
     }
 })("docReady", window);
+
+
+
+
+//// UIKIT UTIL
+
+
+// TODO(vivek): need a new name for my rect class. 
+// TODO(vivek): attempt adopting a more javascripty approach to rects. 
+// TODO(vivek): consider removing namespace to make more javascripty.
+
+
+//! CGRect
+function CGRectZero() {
+    return CGRectMake(0, 0, 0, 0);
+}
+
+function CGRectNull() {
+    return CGRectMake(null, null, null, null);
+}
+
+function CGRectMake(x, y, width, height) {
+    return {
+        origin: {x: x, y: y},
+        size: {width: width, height: height}
+    }
+}
+
+function CGRectFromBBox(bbox) {
+    return CGRectMake(bbox.left, bbox.top, bbox.width, bbox.height);
+}
+
+function CGRectInset(rect, xInset, yInset) {
+    if (!rect) {
+        return;
+    }
+    var newRect = CGRectCopy(rect);
+    newRect.origin.x += xInset;
+    newRect.origin.y += yInset;
+    newRect.size.width -= 2 * xInset;
+    newRect.size.height -= 2 * yInset;
+    return newRect
+}
+
+function CGRectRelativeToRect(sourceRect, relativeRect) {
+    var newRect = CGRectCopy(relativeRect);
+    newRect.origin.x -= sourceRect.origin.x;
+    newRect.origin.y -= sourceRect.origin.y;
+    return newRect;
+}
+
+function CGRectIntersect(rectA, rectB) {
+    throw 'NOT IMPLEMENTED';
+}
+
+function CGRectCopy(rect) {
+    return JSON.parse(JSON.stringify(rect));
+}
+
+function CGPointMake(x, y) {
+    return {x: x, y: y}
+}
+
+function CGPointZero() {
+    return CGPointMake(0, 0);
+}
+
+function CGTransformIdentity() {
+    return [1, 0, 0, 1, 0, 0];
+}
+
+function CGTransformTranslate(tx, ty) {
+    return [1, 0, 0, 1, tx, ty];
+}
+
+function CGTransformScale(sx, sy) {
+    return [sx, 0, 0, sy, 0, 0];
+}
+
+function CGTransformRotate(angle) {
+    return [Math.cos(angle), Math.sin(angle), -Math.sin(angle), Math.cos(angle), 0, 0];
+}
+
+
+function CGTransformConcat(t1, t2) {
+    var m1 = _matrixFromTransform(t1);
+    var m2 = _matrixFromTransform(t2);
+    return _transformFromMatrix(_matrixMultiply(m1, m2));
+}
+
+function _matrixFromTransform(t) {
+    var a = t[0];
+    var b = t[1];
+    var c = t[2];
+    var d = t[3];
+    var e = t[4];
+    var f = t[5];
+
+    return [
+        [a, c, e],
+        [b, d, f],
+        [0, 0, 1],
+    ];
+}
+
+function _transformFromMatrix(m) {
+    var a = m[0][0];
+    var b = m[1][0];
+    var c = m[0][1];
+    var d = m[1][1];
+    var e = m[0][2];
+    var f = m[1][2];
+    return [a, b, c, d, e, f];
+}
+
+// return A * B
+function _matrixMultiply(a, b) {
+  var aNumRows = a.length, aNumCols = a[0].length,
+      bNumRows = b.length, bNumCols = b[0].length,
+      m = new Array(aNumRows);  // initialize array of rows
+  for (var r = 0; r < aNumRows; ++r) {
+    m[r] = new Array(bNumCols); // initialize the current row
+    for (var c = 0; c < bNumCols; ++c) {
+      m[r][c] = 0;             // initialize the current cell
+      for (var i = 0; i < aNumCols; ++i) {
+        m[r][c] += a[r][i] * b[i][c];
+      }
+    }
+  }
+  return m;
+}
+
+
+//! Dom Node Helpers
+
+function setFrame(node, rect) {
+    if (!node) {
+        return;
+    }
+
+    node.style.top = rect.origin.y;
+    node.style.left = rect.origin.x;
+    node.style.width = rect.size.width;
+    node.style.height = rect.size.height;
+
+    node.height = rect.size.height;
+    node.width = rect.size.width;
+}
+
+// TODO(vivek): frame of document.body should be the entire size. its bounds should take care of the visibleRect being smaller. Not sure how to handle resizing a document, obviously subelements should adapt their sizes to fit. 
+function getFrame(node) {
+    // TODO(vivek): there is no concept of a transform so this is easy.
+    if (!node || node == document) {
+        return CGRectNull();
+    }
+    else if (node == document.body.parentNode) {
+        var bbox = node.getBoundingClientRect();
+        return CGRectMake(bbox.left, bbox.top, node.clientWidth, node.clientHeight);
+    }
+    else {
+        var absoluteRect = CGRectFromBBox(node.getBoundingClientRect());
+        if (node == document.body) {
+            absoluteRect.size.height = node.clientHeight;
+            absoluteRect.size.width = node.clientWidth;
+        }
+
+        var parentRect = CGRectFromBBox(node.parentNode.getBoundingClientRect());
+        return CGRectRelativeToRect(parentRect, absoluteRect);
+    }
+}
+
+function getBounds(node) {
+    var style = window.getComputedStyle(node);
+    var inset = 0;
+    inset += parseFloat(style.margin);
+    inset += parseFloat(style.borderWidth);
+    inset += parseFloat(style.padding);
+
+    var frame = getFrame(node);
+    frame = CGRectInset(frame, inset, inset);
+    frame.origin = {x:0, y:0};
+
+    return frame;
+}
+
+function AnimationCurveInOutQuad(t)
+{
+    if(t <= 0.5)
+        return 2.0 * t * t;
+    t -= 0.5;
+    return 2.0 * t * (1.0 - t) + 0.5;
+}
+
+
+
+
+
+
+
+
+
